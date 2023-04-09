9011f01ac8f8baf73ade855eefe7fab4d92ebefa
sho-he <s.n.0704.g@gmail.com>
2023-04-09 22:52:31 +0900

    SIMPLE呼び出し修正

--- app/main.go
+++ app/main.go
@@ -3,8 +3,7 @@ package main
 import "golang_computation/expression"
 
 func main () {
-		a := simple.Add{Left: simple.Number{Value: 1}, Right: simple.Number{Value: 2}}
-		a.To_s()
-		n := simple.Number{Value: 1}
-		n.Inspect()
+		result := simple.Add{Left: simple.Number{Value: 1}, Right: simple.Number{Value: 2}}
+		result.To_s()
+		result.Inspect()
 }
