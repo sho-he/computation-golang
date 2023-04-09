1aeaaa24e402efd8c8b861cf14d3b99daf144818
sho-he <s.n.0704.g@gmail.com>
2023-04-09 22:11:08 +0900

    SIMPLEの呼び出し方を分かりやすくした

--- app/expression/simple.go
+++ app/expression/simple.go
@@ -9,38 +9,48 @@ type Number struct {
 	Value int
 }
 
-func (n *Number) To_s() string {
+func (n Number) To_s() string {
 	return strconv.Itoa(n.Value)
 }
 
-func  (n *Number) Inspect() {
-	fmt.Printf("<<# {%T} >>", n.self)
+func  (n Number) Inspect() string {
+	t := fmt.Sprintf("<< %T >>", n)
+	fmt.Println(t)
+	return t
 }
 
 
 type Add struct {
-	Left int
-	Right int
+	Left Number
+	Right Number
 }
 
-func (a *Add) To_s() string {
-	return fmt.Sprintf("%d + %d", a.Left, a.Right)
+func (a Add) To_s() string {
+	expression := fmt.Sprintf("%d + %d", a.Left.Value, a.Right.Value)
+	fmt.Println(expression)
+	return expression
 }
 
-func (a *Add) Inspect() {
-	fmt.Printf("<<# {%T} >>", a)
+func (a Add) Inspect() string {
+	t := fmt.Sprintf("<< %T >>", a)
+	fmt.Println(t)
+	return t
 }
 
 
 type Multiply struct {
-	Left int
-	Right int
+	Left Number
+	Right Number
 }
 
-func (m *Multiply) to_s() string {
-	return fmt.Sprintf("%d * %d", m.Left, m.Right)
+func (m Multiply) to_s() string {
+	expression := fmt.Sprintf("%d * %d", m.Left.Value, m.Right.Value)
+	fmt.Println(expression)
+	return expression
 }
 
-func (m *Multiply) Inspect() {
-	fmt.Printf("<<# {%T} >>", m)
+func (m Multiply) Inspect() string {
+	t := fmt.Sprintf("<< %T >>", m)
+	fmt.Println(t)
+	return t
 }
