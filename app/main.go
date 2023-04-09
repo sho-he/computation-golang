2acb3cc7e39d8448a5f8cbaf104aac4325966786
sho-he <s.n.0704.g@gmail.com>
2023-04-09 22:11:33 +0900

    SIMPLEのexpressionを呼び出す土台を整理した

--- app/main.go
+++ app/main.go
@@ -3,6 +3,8 @@ package main
 import "golang_computation/expression"
 
 func main () {
+		a := simple.Add{Left: simple.Number{Value: 1}, Right: simple.Number{Value: 2}}
+		a.To_s()
 		n := simple.Number{Value: 1}
 		n.Inspect()
 }
