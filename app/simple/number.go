c77a88669fc4daf3d554eea74fe2feea190f55ec
sho-he <s.n.0704.g@gmail.com>
2023-04-10 23:22:17 +0900

    Numberコンストラクタを作成

--- app/simple/number.go
+++ app/simple/number.go
@@ -5,15 +5,20 @@ import "fmt"
 
 
 type Number struct {
-	self *Number
 	Value int
 }
 
+func NewNumber(value int) *Number {
+	number := &Number{Value: value}
+	return number
+} 
+
 func (n *Number) To_s() string {
+	fmt.Println(strconv.Itoa(n.Value))
 	return strconv.Itoa(n.Value)
 }
 
-func  (n Number) Inspect() string {
+func  (n *Number) Inspect() string {
 	t := fmt.Sprintf("<< %T >>", n)
 	fmt.Println(t)
 	return t
