166519bb70222a84daa31e77e69432fb23a7d238
sho-he <s.n.0704.g@gmail.com>
2023-04-10 23:40:28 +0900

    Add使った例

--- app/simpleExec.go
+++ app/simpleExec.go
@@ -1,13 +1,9 @@
 package main
 
-import "fmt"
 import "golang_computation/simple"
 
 func main () {
-		result := simple.Add{Left: simple.Number{Value: 1}, Right: simple.Number{Value: 2}}
-		fmt.Println(result)
-		// result2 := simple.Add{Left: simple.Number{Value: 3}, Right: simple.Number{Value: 6}}
-		// result3 := simple.Multiply{Left: result, Right: result2}
-		// result3.To_s()
-		// result.Inspect()
+		result := simple.Add(simple.Number(1), simple.Number(2))
+		result.To_s()
+		result.Inspect()
 }
