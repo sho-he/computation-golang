c3b4ffab3798f156d940bc0594c04c1cd7576ae1
sho-he <s.n.0704.g@gmail.com>
2023-04-15 14:09:29 +0900

    分を利用した例

--- app/simpleExec.go
+++ app/simpleExec.go
@@ -5,4 +5,7 @@ import "golang_computation/simple"
 func main () {
 		simple.Machine(simple.Multiply(simple.Add(simple.Variable("x"), simple.Number(2)),simple.Number(3)),simple.Environment("x", simple.Number(2))).Run()
 		simple.Machine(simple.LessThan(simple.Add(simple.Number(1), simple.Number(2)), simple.Number(3)),simple.Environment("x", simple.Number(2))).Run()
+
+		statement := simple.Assign("x", simple.Add(simple.Variable("x"), simple.Number(1)))
+		statement.Inspect()
 }
